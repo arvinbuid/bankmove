bff31b78cd07ac22600ea2697a71499546a2b48f
Arvin Buid <arvinbuid01@gmail.com>
2024-09-06 10:59:40 +0800

    Create formatMovementDate() to display a message 'Today', 'Yesterday', '7 days ago' if the transaction is less than 7 days when it actually occur or else display the entire date in a YYYY/MM/22, HH:SE format in the UI.

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2017-11-18T21:31:17.178Z',
+    '2018-12-23T07:42:02.383Z',
+    '2022-01-28T09:15:04.904Z',
+    '2024-04-01T10:17:24.185Z',
+    '2024-08-27T14:11:59.604Z',
+    '2024-09-02T17:01:17.194Z',
+    '2024-09-03T23:36:17.929Z',
+    '2024-09-05T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -28,14 +28,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2022-11-01T13:15:33.035Z',
+    '2023-11-30T09:48:16.867Z',
+    '2023-12-25T06:04:23.907Z',
+    '2024-01-25T14:18:46.235Z',
+    '2024-02-05T16:33:06.386Z',
+    '2024-04-10T14:43:26.374Z',
+    '2024-06-25T18:49:59.371Z',
+    '2024-07-26T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -69,6 +69,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// FUNCTIONS
+
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago `;
+
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+
+  return `${year}/${month}/${day}`;
+};
+
 // Display deposit & withdrawals
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
@@ -84,13 +103,7 @@ const displayMovements = function (acc, sort = false) {
 
     // display date
     const date = new Date(acc.movementsDates[i]);
-
-    const year = date.getFullYear();
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const dateMonth = `${date.getDate()}`.padStart(2, 0);
-
-    const displayDate =
-      (labelDate.textContent = `${year}/${month}/${dateMonth}`);
+    const displayDate = formatMovementDate(date);
 
     const html = `
      <div class="movements__row">
@@ -652,3 +665,13 @@ btnSort.addEventListener('click', function (e) {
 
 // future.setFullYear(2050);
 // console.log(future);
+
+// const future = new Date(2029, 11, 1, 22, 0, 0);
+// console.log(+future);
+
+// const calcDaysPassed = (date1, date2) =>
+//   Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+// const days1 = calcDaysPassed(new Date(2040, 4, 22), new Date(2040, 4, 12));
+
+// console.log('Days:', days1);
